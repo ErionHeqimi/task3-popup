c451051133965be629a8658d89908a051f4f27a4
ErionHeqimi <121134554+ErionHeqimi@users.noreply.github.com>
2024-02-29 00:30:52 +0100

    Css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './PopupComponent.css';
 
-function App() {
+const PopupComponent = ({ onClose }) => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleStartButtonClick = () => {
+    setShowPopup(true);
+  };
+
+ 
+
+  const handleSaveButtonClick = () => {
+    
+    setShowPopup(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!showPopup && (
+        <button className="start-button" onClick={handleStartButtonClick}>Start</button>
+      )}
+      {showPopup && (
+        <div className="popup-container">
+          <div className="popup">
+            <div className="popup-header">
+              <h3 className="theme-color">Theme Color</h3>
+              <p id='theme'>Change theme</p>
+            </div>
+            <div className="popup-content">
+              <div className="popup-row">
+                <span>Font Color</span>
+                <div className='together'>
+                  <span>#44444</span>
+                  <div className="color-box"></div>
+                </div>
+              </div>
+              <div className="popup-row">
+                <span>Background Color</span>
+                <div className='together'>
+                  <span>#44444</span>
+                  <div className="color-boxW"></div>
+                </div>
+              </div>
+              <div className="popup-row">
+                <span>Button Color</span>
+                <div className='together'>
+                  <span>#44444</span>
+                  <div className="color-boxB"></div>
+                </div>
+              </div>
+              <div className="popup-row">
+                <span>Button Border Color</span>
+                <div className='together'>
+                  <span>#44444</span>
+                  <div className="color-boxB"></div>
+                </div>
+              </div>
+              <div className="popup-row">
+                <span>Buttons Mouseover Color</span>
+                <div className='together'>
+                  <span>#44444</span>
+                  <div className="color-boxBB"></div>
+                </div>
+              </div>
+            </div>
+            <div className="popup-footer">
+              <button className="cancel-button" onClick={handleSaveButtonClick}>Cancel</button>
+              <button className="save-button" onClick={handleSaveButtonClick}>Save</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
-export default App;
+export default PopupComponent;
